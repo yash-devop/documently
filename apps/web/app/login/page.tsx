b7aeafc8994e91de9fe1a1830d1b48c958f8427f
yash-devop <yashkamble.dev@gmail.com>
2026-08-30 13:19:57 +0530

    feat(web,ui): rebuild login page with field/label/separator components and add tabler icons

--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -3,7 +3,22 @@
 import { useState } from "react";
 import { authClient } from "../../lib/better-auth";
 import { useRouter } from "next/navigation";
-import { Input, Button } from "@repo/ui";
+import {
+  Input,
+  Button,
+  Field,
+  FieldSet,
+  FieldLabel,
+  FieldDescription,
+  FieldGroup,
+  FieldSeparator,
+  Separator,
+} from "@repo/ui";
+import {
+  IconBrandGithubFilled,
+  IconBrandGoogle,
+  IconBrandGoogleFilled,
+} from "@tabler/icons-react";
 
 export default function LoginPage() {
   const router = useRouter();
@@ -40,180 +55,82 @@ export default function LoginPage() {
     }
   };
   return (
-    <div className="max-w-4xl mx-auto flex flex-col gap-10">
-      <div className="mx-auto flex w-full items-start justify-center gap-14 pt-32">
-        {/* Default */}
-        <div className="flex w-fit flex-col items-start gap-3">
-          <Button variant="default" size="default">
-            Default
-          </Button>
-          <Button variant="default" size="xs">
-            Default
-          </Button>
-          <Button variant="default" size="sm">
-            Default
-          </Button>
-          <Button variant="default" size="md">
-            Default
-          </Button>
-          <Button variant="default" size="lg">
-            Default
-          </Button>
-          <Button variant="default" size="icon">
-            D
-          </Button>
-        </div>
-
-        {/* Secondary */}
-        <div className="flex w-fit flex-col items-start gap-3">
-          <Button variant="secondary" size="default">
-            Secondary
-          </Button>
-          <Button variant="secondary" size="xs">
-            Secondary
-          </Button>
-          <Button variant="secondary" size="sm">
-            Secondary
-          </Button>
-          <Button variant="secondary" size="md">
-            Secondary
-          </Button>
-          <Button variant="secondary" size="lg">
-            Secondary
-          </Button>
-          <Button variant="secondary" size="icon">
-            S
-          </Button>
-        </div>
-
-        {/* Outline */}
-        <div className="flex w-fit flex-col items-start gap-3">
-          <Button variant="outline" size="default">
-            Outline
-          </Button>
-          <Button variant="outline" size="xs">
-            Outline
-          </Button>
-          <Button variant="outline" size="sm">
-            Outline
-          </Button>
-          <Button variant="outline" size="md">
-            Outline
-          </Button>
-          <Button variant="outline" size="lg">
-            Outline
-          </Button>
-          <Button variant="outline" size="icon">
-            O
-          </Button>
-        </div>
-
-        {/* Ghost */}
-        <div className="flex w-fit flex-col items-start gap-3">
-          <Button variant="ghost" size="default">
-            Ghost
-          </Button>
-          <Button variant="ghost" size="xs">
-            Ghost
-          </Button>
-          <Button variant="ghost" size="sm">
-            Ghost
-          </Button>
-          <Button variant="ghost" size="md">
-            Ghost
-          </Button>
-          <Button variant="ghost" size="lg">
-            Ghost
-          </Button>
-          <Button variant="ghost" size="icon">
-            G
-          </Button>
-        </div>
-
-        {/* Link */}
-        <div className="flex w-fit flex-col items-start gap-3">
-          <Button variant="link" size="default">
-            Link
-          </Button>
-          <Button variant="link" size="xs">
-            Link
-          </Button>
-          <Button variant="link" size="sm">
-            Link
-          </Button>
-          <Button variant="link" size="md">
-            Link
-          </Button>
-          <Button variant="link" size="lg">
-            Link
-          </Button>
-          <Button variant="link" size="icon">
-            L
-          </Button>
-        </div>
-
-        {/* Destructive */}
-        <div className="flex w-fit flex-col items-start gap-3">
-          <Button variant="destructive" size="default">
-            Destructive
-          </Button>
-          <Button variant="destructive" size="xs">
-            Destructive
-          </Button>
-          <Button variant="destructive" size="sm">
-            Destructive
-          </Button>
-          <Button variant="destructive" size="md">
-            Destructive
-          </Button>
-          <Button variant="destructive" size="lg">
-            Destructive
-          </Button>
-          <Button variant="destructive" size="icon">
-            D
-          </Button>
-          <Button variant="destructive" size="icon-sm">
-            D
-          </Button>
-        </div>
+    <div className="max-w-md mx-auto mt-40">
+      <FieldSet className="w-full ">
+        <FieldGroup>
+          <Field>
+            <FieldLabel htmlFor="name">Username</FieldLabel>
+            <Input
+              id="name"
+              type="text"
+              placeholder="Max Leiter"
+              onChange={(e) =>
+                setCreds((prev) => ({ ...prev, name: e.target.value }))
+              }
+            />
+          </Field>
+          <Field>
+            <FieldLabel htmlFor="email">Email</FieldLabel>
+            <Input
+              id="email"
+              type="text"
+              placeholder="max@leiter.com"
+              onChange={(e) =>
+                setCreds((prev) => ({ ...prev, email: e.target.value }))
+              }
+            />
+          </Field>
+          <Field>
+            <FieldLabel htmlFor="password">Password</FieldLabel>
+            <Input
+              id="password"
+              type="password"
+              placeholder="••••••••"
+              onChange={(e) =>
+                setCreds((prev) => ({ ...prev, password: e.target.value }))
+              }
+            />
+          </Field>
+          <Field>
+            <Button
+              onClick={handleGoogle}
+              variant="default"
+              className={"w-full"}
+            >
+              Sign up
+            </Button>
+          </Field>
+        </FieldGroup>
+      </FieldSet>
+      <div className="flex items-center gap-3 py-4">
+        <Separator className="flex-1 h-[0.5px] bg-foreground-lighter/30" />
+        <span className="text-xs text-foreground-lighter uppercase">or</span>
+        <Separator className="flex-1 h-[0.5px] bg-foreground-lighter/30" />
       </div>
-      <div className="mx-auto flex flex-col w-100 items-start justify-center gap-2">
-        <Input placeholder="Enter Placeholder" />
-        <Input type="file" placeholder="Enter Placeholder" />
-        <Input type="text" placeholder="Enter Placeholder" disabled={true} />
-      </div>
+      <FieldSet>
+        <FieldGroup className="flex flex-row gap-2 ">
+          <Field orientation={"horizontal"}>
+            <Button
+              onClick={handleGoogle}
+              variant="secondary"
+              className={"w-full"}
+            >
+              <IconBrandGoogleFilled />
+              Google
+            </Button>
+          </Field>
+          <Field orientation={"horizontal"}>
+            <Button
+              onClick={handleGithub}
+              variant="secondary"
+              className="gap-1.5 w-full"
+            >
+              <IconBrandGithubFilled />
+              Github
+            </Button>
+          </Field>
+        </FieldGroup>
+      </FieldSet>
     </div>
   );
-}
-
-{
-  /* <div>
-      <Button onClick={handleGoogle}> GOOGLE</Button>
-      <Button onClick={handleGithub}> GITHUB</Button>
-      <div className="max-w-xl mx-auto">
-        Email/Password
-        <Input
-          type="text"
-          placeholder="Name"
-          onChange={(e) =>
-            setCreds((prev) => ({ ...prev, name: e.target.value }))
-          }
-        />
-        <Input
-          type="email"
-          placeholder="Email"
-          onChange={(e) =>
-            setCreds((prev) => ({ ...prev, email: e.target.value }))
-          }
-        />
-        <Input
-          type="password"
-          placeholder="password"
-          onChange={(e) =>
-            setCreds((prev) => ({ ...prev, password: e.target.value }))
-          }
-        />
-        <Button onClick={handleEmailSignUp}>Signup</Button>
-      </div>
-    </div> */
 }
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,2 +1,5 @@
 export * from "./components/ui/input";
 export * from "./components/ui/button";
+export * from "./components/ui/field";
+export * from "./components/ui/label";
+export * from "./components/ui/separator";
